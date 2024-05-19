644bb10048bff22fb063e984e80735660f588c35
Knut Behrends <knb@gfz-potsdam.de>
2024-05-19 17:34:20 +0200

    Add new prompts to personas-grouped-sorted.csv file

---
+++ scripts/preprocess-anthropic.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Preprocess the anthropic-prompts.json file
+
+# classification categories:
+ csvlook -K 1   -y 3  < personas-grouped-sorted.csv | csvcut -d\| -c 2 -H | sort -u | grep "^ "
+
+# ...classification was done by chatgpt-4o... - manual, interactive process
+
+# re-order the columns and create a new CSV file
+< anthropic-prompts-classif.json \
+    jq -r  ' sort_by(.[0], .[1]) | map([.[0], .[1] + ": " + .[2], .[3]]) | .[] | @csv ' \
+> anthropic-prompts-classif.csv
